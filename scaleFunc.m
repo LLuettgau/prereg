0beaa1e98f2d3cc1eb535b78faa054fda0fa8fcc
LLuettgau <lennart.luettgau@gmail.com>
2023-01-18 10:38:30 +0000

    Create scaleFunc.m

---
+++ scaleFunc.m
@@ -0,0 +1,10 @@
+function [newX] = scaleFunc(X)
+
+%scaleFact = 0.6*max(max(X(:)),-min(X(:)));  % another way to do it
+%scaleFact = max(abs(X(:)));
+scaleFact = prctile(abs(X(:)),95); %originally implemented way
+
+newX = X ./ scaleFact;
+
+
+end
